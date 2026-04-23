857614961d1bb777b86528aadf767aad96b5f4b0
Dylan B. <dylanb92010@gmail.com>
2026-04-23 10:57:22 -0400

    Small drivetrain tweaks (#82)
    * Major overhaul of constants and swerve, bug fixes
    
    * Small drivetrain tweaks
    
    * Remove an override in swerve
    
    * Remove more overrides in swerve

--- TeamCode/src/main/java/drivetrains/Mecanum.java
+++ TeamCode/src/main/java/drivetrains/Mecanum.java
@@ -41,15 +41,6 @@ public class Mecanum extends Drivetrain {
         brMotor = this.constants.brData.build(hardwareMap);
     }
 
-    @Override
-    protected void setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior behavior) {
-        flMotor.setZeroPowerBehavior(behavior);
-        blMotor.setZeroPowerBehavior(behavior);
-        frMotor.setZeroPowerBehavior(behavior);
-        brMotor.setZeroPowerBehavior(behavior);
-    }
-
-    @Override
     protected boolean isRobotCentric() { return constants.robotCentric; }
 
     /**
@@ -78,7 +69,6 @@ public class Mecanum extends Drivetrain {
         brMotor.setPower(brPower);
     }
 
-    @Override
     public void moveWithVectors(double drive, double strafe, double turn) {
         double flPower = drive + strafe + turn;
         double blPower = drive - strafe + turn;
@@ -88,12 +78,10 @@ public class Mecanum extends Drivetrain {
         setPowers(flPower, blPower, frPower, brPower);
     }
 
-    @Override
     public void stop() {
         setPowers(0, 0, 0, 0);
     }
 
-    @Override
     public void debug(Telemetry telemetry) {
         telemetry.addData("Front Left Power", flMotor.getPower());
         telemetry.addData("Front Right Power", frMotor.getPower());
